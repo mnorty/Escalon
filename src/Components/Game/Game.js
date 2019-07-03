09aecf62295db2a88c43bd8ea3abb7340a61a37b
shawn's macbook <shawnscivally@shawn-ski.local>
2019-07-03 10:41:24 -0600

    ready to merge

---
+++ src/Components/Game/Game.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Game.css';
+
+class Game extends Component {
+
+    render() {
+        return (
+            <div className='playCont'>
+                <h1>Game</h1>
+            </div>
+        )
+    }
+}
+
+export default Game;
