f93e6f029d5437f486a9aad2d8d3a6e18c938e99
mnorty <mnorty@gmail.com>
2019-07-09 10:14:38 -0600

    feat: Display now toggles

--- src/Components/GameCentral/GameDisplay/GameDisplayCard.js
+++ src/Components/GameCentral/GameDisplay/GameDisplayCard.js
@@ -4,6 +4,12 @@ import './GameDisplayCard.css'
 import axios from 'axios'
 
 class Card extends Component {
+  constructor(){
+    super()
+    this.state = {
+      shareDisplay: 'false'
+    }
+  }
 
   handleDeleteGame = () => {
     axios.delete(`/deletegame/${this.props.game.id}`)
@@ -14,18 +20,36 @@ class Card extends Component {
   render() {
     const { game_title,game_intro,gameroom_id} = this.props.game;
 
+    const toggleShareModal = () => {
+      if(
+        this.state.shareDisplay === 'true'
+      ){this.setState({
+        shareDisplay:'false'
+      })}else {
+        this.setState({
+          shareDisplay:'true'
+        })
+      } 
+    }
+
 
     return (      
     <div  className='cardContainer'>
     <p>Game Intro: {game_intro}</p>
     <p>Game Name: {game_title}</p>
-    <p>Game Room id:{gameroom_id}</p>
     <button className='cardBtn'>Edit</button>
-    <button className='cardBtn'>Share</button>
+    <button
+    className='cardBtn'
+    onClick={toggleShareModal}
+    >Share</button>
     <button 
     className='cardBtn'
     onClick={this.handleDeleteGame}
     >Delete</button>
+    {this.state.shareDisplay !== 'false'
+              ? <div id='modal' >Game Room id:{gameroom_id}</div>
+              : null
+            }
   </div>
 )
 }
